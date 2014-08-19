916a90c024d59861c1dac322004316413fc194ca
Chase Conklin <chase@conklins.net>
2014-08-19 12:19:07 -0400

    made the example include all tests, not those that ran

--- lib/tester/suite.rb
+++ lib/tester/suite.rb
@@ -32,7 +32,7 @@ module Tester
       ignored.each.with_index do |test, index|
         Reporter.display index + 1, test.epilogue
       end
-      Reporter.epilogue ran.count, failures.count, skipped.count, ignored.count
+      Reporter.epilogue all_tests.count, failures.count, skipped.count, ignored.count
       puts
     end
     def all_tests
@@ -46,12 +46,6 @@ module Tester
     end
     def ignored
       @contexts.map(&:ignored).inject(:+)
-    end
-    def ran
-      @contexts.map(&:ran).inject(:+)
-    end
-    def passed
-      @contexts.map(&:passed).inject(:+)
     end
   end
 end
